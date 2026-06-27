6f22bdf47ca1be70f2979efc9508fab54439ad64
yashrajput33333 <yashrajput33333@gmail.com>
2026-06-27 19:33:44 +0530

    Remove post added notification handler
    Removed handlePostAdded function and its usage in PostForm.

--- Frontend/src/pages/AddPost.jsx
+++ Frontend/src/pages/AddPost.jsx
@@ -3,14 +3,10 @@ import { Container, PostForm } from "../components";
 import { toast } from "react-toastify";
 
 function AddPost() {
-    const handlePostAdded = () => {
-        toast.success("Post added successfully! 🎉");
-    };
-
     return (
         <div className="py-8">
             <Container>
-                <PostForm onPostAdded={handlePostAdded} />
+                <PostForm />
             </Container>
         </div>
     );
