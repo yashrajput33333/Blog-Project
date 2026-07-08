6d9213dc0ce831a49ec960c87bdf153a89e20eeb
yashrajput33333 <yashrajput33333@gmail.com>
2026-07-08 21:02:28 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -28,45 +28,34 @@ const router = createBrowserRouter([
         {
             path: "/login",
             element: (
-                // <AuthLayout authentication={false}>
                     <Login />
-                // </AuthLayout>
             ),
         },
         {
             path: "/signup",
             element: (
-              //   <AuthLayout authentication={false}>
                     <Signup></Signup>
-              //   </AuthLayout>
             ),
         },
         
         {
             path: "/all-posts",
             element: (
-                // <AuthLayout authentication>
-                //     {" "}
                     <AllPosts />
-                // </AuthLayout>
             ),
         },
         {
             path: "/add-post",
             element: (
-                // <AuthLayout authentication>
-                    // {" "} 
+          
                     <AddPost></AddPost>
-                // </AuthLayout>
             ),
         },
         {
             path: "/edit-post/:slug",
             element: (
-                // <AuthLayout authentication>
-                //     {" "}
+            
                     <EditPost />
-                // </AuthLayout>
             ),
         },
         {
