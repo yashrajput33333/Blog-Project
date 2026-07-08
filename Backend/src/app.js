070a44640538b7970c51bb2f432d0e0041b73a7e
yashrajput33333 <yashrajput33333@gmail.com>
2026-07-08 20:35:45 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,22 +5,10 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-const allowedOrigins = process.env.CORS_ORIGIN
-  ? process.env.CORS_ORIGIN.split(",")
-  : ["http://localhost:5173"];
-//
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
